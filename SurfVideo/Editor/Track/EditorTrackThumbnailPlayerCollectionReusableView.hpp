4a67016063967d13c0c67ceb09fb17e45ebe2b55
Jinwoo Kim <kidjinwoo@me.com>
2024-04-26 02:47:17 +0900

    no message

--- SurfVideo/Editor/Track/EditorTrackThumbnailPlayerCollectionReusableView.hpp
+++ SurfVideo/Editor/Track/EditorTrackThumbnailPlayerCollectionReusableView.hpp
@@ -13,7 +13,6 @@ NS_ASSUME_NONNULL_BEGIN
 __attribute__((objc_direct_members))
 @interface EditorTrackThumbnailPlayerCollectionReusableView : UICollectionReusableView
 @property (class, readonly, nonatomic) NSString *elementKind;
-- (void)updateWithAVAsset:(AVAsset *)avAsset time:(CMTime)time;
 @end
 
 NS_ASSUME_NONNULL_END
